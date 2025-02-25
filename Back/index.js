45a59096cedf8a39115765c083c3dae4b4f9d82d
GiraMorales <gira.morales@gamil.com>
2025-02-25 10:03:21 +0100

    conectar con la BD

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+const { connectDB } = require('./src/config/db');
 
 const app = express();
+
+connectDB();
 
 app.use('*', (req, res, next) => {
   return res.status(404).json('Route Not Found');
