7983af03eba09865aca2a5b7ba6562d941d550a5
GiraMorales <gira.morales@gamil.com>
2025-02-25 10:30:00 +0100

    modelo Schema

---
+++ Back/src/api/models/evento.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const eventoSchema = new mongoose.Schema(
+  {
+    titulo: { type: String, required: true },
+    portada: { type: String, required: true },
+    fecha: { type: Number, required: true },
+    ubicacion: { type: String, required: true },
+    descripcion: { type: String, required: false },
+    relatedUsers: [
+      { type: mongoose.Types.ObjectId, required: false, ref: 'user' }
+    ]
+  },
+  {
+    timestamps: true,
+    collection: 'eventos'
+  }
+);
+
+const Evento = mongoose.model('eventos', eventoSchema, 'eventos');
+module.exports = Evento;
