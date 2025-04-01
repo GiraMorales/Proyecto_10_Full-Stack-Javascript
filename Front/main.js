037b6042e3dac72cb626eda693167b169ff9afc2
GiraMorales <gira.morales@gamil.com>
2025-04-02 00:24:41 +0200

    comint 2

--- Front/main.js
+++ Front/main.js
@@ -1,15 +1,22 @@
 import { Footer } from './src/components/Footer/Footer';
 import { Header } from './src/components/Header/Header';
+import { Home } from './src/pages/Home/Home';
 import '/style.css';
 
 const Main = () => {
   const app = document.getElementById('app');
   app.innerHTML = `
-  <header></header>
-  <main></main>
-  <footer></footer>`;
+    <header></header>
+    <main></main>
+    <footer></footer>
+  `;
+
+  Header();
+  Footer();
 };
 
-Main();
-Header();
-Footer();
+// Esperar a que el DOM esté listo antes de ejecutar Home()
+document.addEventListener('DOMContentLoaded', () => {
+  Main();
+  Home();
+});
